dfed172c0c1e3b62c31cba1855ed23a05ec0c2be
TomChardon <84155625+TomChardon@users.noreply.github.com>
2023-05-28 23:37:32 -0300

    Update employees.sql

--- parte-3/project/retail-data-warehouse/stg/employees.sql
+++ parte-3/project/retail-data-warehouse/stg/employees.sql
@@ -1,1 +1,13 @@
 
+CREATE TABLE [dbo].[employees2](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [varchar](50) NULL,
+	[apellido] [varchar](50) NULL,
+	[fecha_entrada] [date] NULL,
+	[fecha_salida] [date] NULL,
+	[telefono] [varchar](50) NULL,
+	[pais] [varchar](20) NULL,
+	[provincia] [varchar](50) NULL,
+	[codigo_tienda] [varchar](20) NULL,
+	[posicion] [varchar](50) NULL,
+) 
