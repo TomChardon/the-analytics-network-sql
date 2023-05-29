ae785307140bc58300226e99898e50d4ebf951bd
TomChardon <84155625+TomChardon@users.noreply.github.com>
2023-05-28 23:53:40 -0300

    Create store_master.sql

---
+++ parte-3/project/retail-data-warehouse/stg/store_master.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [dbo].[store_master](
+	[codigo_tienda] [smallint] NULL,
+	[pais] [varchar](100) NULL,
+	[provincia] [varchar](100) NULL,
+	[ciudad] [varchar](100) NULL,
+	[direccion] [varchar](255) NULL,
+	[nombre] [varchar](255) NULL,
+	[tipo] [varchar](100) NULL,
+	[fecha_apertura] [date] NULL,
+	[latitud] [decimal](10, 8) NULL,
+	[longitud] [decimal](11, 8) NULL
+) 
