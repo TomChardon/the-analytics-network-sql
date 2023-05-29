0203eaa3bcf7a807ec08055d28213d7213b0b78d
TomChardon <84155625+TomChardon@users.noreply.github.com>
2023-05-28 23:52:31 -0300

    Create product_master.sql

---
+++ parte-3/project/retail-data-warehouse/stg/product_master.sql
@@ -0,0 +1,15 @@
+CREATE TABLE [dbo].[product_master](
+	[codigo_producto] [varchar](255) NULL,
+	[nombre] [varchar](255) NULL,
+	[categoria] [varchar](255) NULL,
+	[subcategoria] [varchar](255) NULL,
+	[subsubcategoria] [varchar](255) NULL,
+	[material] [varchar](255) NULL,
+	[color] [varchar](255) NULL,
+	[origen] [varchar](255) NULL,
+	[ean] [bigint] NULL,
+	[is_active] [varchar](10) NULL,
+	[has_bluetooth] [varchar](10) NULL,
+	[talle] [varchar](255) NULL,
+	[marca] [varchar](50) NULL
+) 
