7196ecffe6b1d565c8915334ae80a41c60d9e628
TomChardon <84155625+TomChardon@users.noreply.github.com>
2023-05-28 23:53:03 -0300

    Create return_movements.sql

---
+++ parte-3/project/retail-data-warehouse/stg/return_movements.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE [dbo].[return_movements](
+	[orden_venta] [nvarchar](255) NULL,
+	[envio] [nvarchar](255) NULL,
+	[item] [nvarchar](255) NULL,
+	[cantidad] [float] NULL,
+	[id_movimiento] [float] NULL,
+	[desde] [nvarchar](255) NULL,
+	[hasta] [nvarchar](255) NULL,
+	[recibido_por] [nvarchar](255) NULL,
+	[fecha] [nvarchar](255) NULL
+)
