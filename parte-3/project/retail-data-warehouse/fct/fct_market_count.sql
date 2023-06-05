a0e1ad6e355a1dc7e40f33b7e2e2770e5802bf24
TomChardon <84155625+TomChardon@users.noreply.github.com>
2023-06-04 23:21:31 -0300

    Update fct_market_count.sql

--- parte-3/project/retail-data-warehouse/fct/fct_market_count.sql
+++ parte-3/project/retail-data-warehouse/fct/fct_market_count.sql
@@ -1,7 +1,8 @@
 CREATE TABLE [dbo].[market_count](
 	[id_tienda] INT IDENTITY (1,1) NOT NULL,
-	[tienda] INT FOREIGN KEY REFERENCES store_master ([codigo_tienda]),
+	[tienda] INT NOT NULL,
 	[fecha] [int] NULL,
 	[conteo] [smallint] NULL,
+	CONSTRAINT FK_marketcount_tienda FOREIGN KEY ([tienda]) REFERENCES store_master ([codigo_tienda]),
 	PRIMARY KEY CLUSTERED ([id_tienda])
 )
