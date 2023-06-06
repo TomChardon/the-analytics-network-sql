3304d5a57c86f1a09e3d8651e608b5ee892c6a7f
TomChardon <84155625+TomChardon@users.noreply.github.com>
2023-06-05 23:28:36 -0300

    Update dim_cost.sql

--- parte-3/project/retail-data-warehouse/dim/dim_cost.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_cost.sql
@@ -2,6 +2,6 @@ CREATE TABLE [dbo].[cost](
 	[id_codigo_producto] int IDENTITY (1,1) NOT NULL,
 	[codigo_producto] [varchar](255) NOT NULL,
 	[costo_promedio_usd] [float] NULL,
-	CONSTRAINT FK_cost_producto FOREIGN KEY ([codigo_producto]) REFERENCES store_master ([codigo_producto]),
+	CONSTRAINT FK_cost_producto FOREIGN KEY ([codigo_producto]) REFERENCES product_master ([codigo_producto]),
 	PRIMARY KEY CLUSTERED ([id_codigo_producto])
 )
