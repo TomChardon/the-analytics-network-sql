dcb9b4ad4c822428df16cee0a0eed456c2e794b4
TomChardon <84155625+TomChardon@users.noreply.github.com>
2023-06-08 00:12:58 -0300

    Update fct_return_movements.sql

--- parte-3/project/retail-data-warehouse/fct/fct_return_movements.sql
+++ parte-3/project/retail-data-warehouse/fct/fct_return_movements.sql
@@ -1,5 +1,5 @@
 CREATE TABLE [dbo].[return_movements](
-    [id_orden_item] INT IDENTITY (1,1) NOT NULL,
+    	[id] INT IDENTITY (1,1) NOT NULL,
 	[orden_venta] [nvarchar](255) NULL,
 	[envio] [nvarchar](255) NULL,
 	[item] [varchar](255) NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE [dbo].[return_movements](
 	[hasta] [nvarchar](255) NULL,
 	[recibido_por] [nvarchar](255) NULL,
 	[fecha] [nvarchar](255) NULL,
-	CONSTRAINT FK_returnmovements_producto FOREIGN KEY ([item]) REFERENCES product_master ([codigo_producto]),
-	PRIMARY KEY CLUSTERED ([id_orden_item])
+	CONSTRAINT id_returnmovements_producto FOREIGN KEY ([item]) REFERENCES product_master ([codigo_producto]),
+	PRIMARY KEY CLUSTERED ([id])
 ) 
 
