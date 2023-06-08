e358eea13e615a34bcb22a95c3c6948a6e418f54
TomChardon <84155625+TomChardon@users.noreply.github.com>
2023-06-08 00:10:48 -0300

    Update dim_suppliers.sql

--- parte-3/project/retail-data-warehouse/dim/dim_suppliers.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_suppliers.sql
@@ -1,8 +1,8 @@
 CREATE TABLE [dbo].[suppliers](
-	[id_proveedor] INT IDENTITY(1,1) NOT NULL,
+	[id] INT IDENTITY(1,1) NOT NULL,
 	[codigo_producto] [varchar](255),
 	[nombre] [nvarchar](255) NULL,
 	[is_primary] [nvarchar](255) NULL,
-	CONSTRAINT FK_suppliers_producto FOREIGN KEY ([codigo_producto]) REFERENCES product_master ([codigo_producto]),
-	PRIMARY KEY CLUSTERED ([id_proveedor])
+	CONSTRAINT id_suppliers_producto FOREIGN KEY ([codigo_producto]) REFERENCES product_master ([codigo_producto]),
+	PRIMARY KEY CLUSTERED ([id])
 )
