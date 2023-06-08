6d2fadf6fa63eb2c98cb4d3aa65ee48a018c1888
TomChardon <84155625+TomChardon@users.noreply.github.com>
2023-06-08 00:13:27 -0300

    Update fct_super_store_count.sql

--- parte-3/project/retail-data-warehouse/fct/fct_super_store_count.sql
+++ parte-3/project/retail-data-warehouse/fct/fct_super_store_count.sql
@@ -1,8 +1,8 @@
 CREATE TABLE [dbo].[super_store_count](
-	[id_tienda] INT IDENTITY (1,1) NOT NULL,
+	[id] INT IDENTITY (1,1) NOT NULL,
 	[tienda] INT NOT NULL,
 	[fecha] [int] NULL,
 	[conteo] [smallint] NULL,
-	CONSTRAINT FK_superstorecount_tienda FOREIGN KEY ([tienda]) REFERENCES store_master ([codigo_tienda]),
-	PRIMARY KEY CLUSTERED ([id_tienda])
+	CONSTRAINT id_superstorecount_tienda FOREIGN KEY ([tienda]) REFERENCES store_master ([codigo_tienda]),
+	PRIMARY KEY CLUSTERED ([id])
 )
