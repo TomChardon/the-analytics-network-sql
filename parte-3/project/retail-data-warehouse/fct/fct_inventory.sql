0331aa4e5f8e5a5f23e05fb8c9754063f53d4849
TomChardon <84155625+TomChardon@users.noreply.github.com>
2023-06-08 00:11:27 -0300

    Update fct_inventory.sql

--- parte-3/project/retail-data-warehouse/fct/fct_inventory.sql
+++ parte-3/project/retail-data-warehouse/fct/fct_inventory.sql
@@ -1,10 +1,10 @@
 CREATE TABLE [dbo].[inventory](
-	[id_sku_fecha] INT IDENTITY (1,1) NOT NULL,
+	[id] INT IDENTITY (1,1) NOT NULL,
 	[tienda] INT NOT NULL, 
 	[sku] [varchar](10) NULL,
 	[fecha] [date] NULL,
 	[inicial] [smallint] NULL,
 	[final] [smallint] NULL,
-	CONSTRAINT FK_inventory_tienda FOREIGN KEY ([tienda]) REFERENCES store_master ([codigo_tienda]),
-	PRIMARY KEY CLUSTERED ([id_sku_fecha])
+	CONSTRAINT id_inventory_tienda FOREIGN KEY ([tienda]) REFERENCES store_master ([codigo_tienda]),
+	PRIMARY KEY CLUSTERED ([id])
 ) 
