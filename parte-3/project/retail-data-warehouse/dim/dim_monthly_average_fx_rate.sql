63a64896c7f931cf02699bfae86415f0cd0c13a5
TomChardon <84155625+TomChardon@users.noreply.github.com>
2023-06-08 00:09:17 -0300

    Update dim_monthly_average_fx_rate.sql

--- parte-3/project/retail-data-warehouse/dim/dim_monthly_average_fx_rate.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_monthly_average_fx_rate.sql
@@ -1,8 +1,8 @@
 CREATE TABLE [dbo].[monthly_average_fx_rate](
-	[id_año_mes] INT IDENTITY (1,1) NOT NULL,
+	[id] INT IDENTITY (1,1) NOT NULL,
 	[mes] [date] NULL,
 	[cotizacion_usd_peso] [decimal](18, 0) NULL,
 	[cotizacion_usd_eur] [decimal](18, 0) NULL,
 	[cotizacion_usd_uru] [decimal](18, 0) NULL,
-	PRIMARY KEY CLUSTERED ([id_año_mes])
+	PRIMARY KEY CLUSTERED ([id])
 )
